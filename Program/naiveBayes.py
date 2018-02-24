084b0598b46a1935daaa7ac1aae6b68c6ecf2e92
MehakBeri <mehakberi1996@gmail.com>
2018-02-24 03:01:29 -0600

    training nb multinomial

--- Program/naiveBayes.py
+++ Program/naiveBayes.py
@@ -29,11 +29,11 @@ getText functions gets the text included in a file specified by the given filena
 '''
 def getText(filename):
     with open(filename, 'r', encoding="Latin-1") as myfile:
-        data=myfile.read().replace('\n', '')
+        data=myfile.read().replace('\n', ' ')
     return data
 
 '''
-TrainMultinomialNB trains a machine learning model using the given training data specified as documents D, and classes are specified in class C
+TrainMultinomialNB trains a machine learning model using the given training data specified as document tuple D, where D[0] is ham, and D[1] is spam, and classes are specified in class C
 '''
 def TrainMultinomialNB(C,D):
     v = extractVocabulary(D)
@@ -46,18 +46,38 @@ def TrainMultinomialNB(C,D):
         Nc = countDocsInClass(D,c)
         prior[c]= Nc/n
         text[c]=ConcatenateTextOfAllDocsInClass(D,c)
+        s=0
         for token in v:
             t[c+'.'+token]=CountTokensOfTerm(text[c],token)
-        denominator = findSmoothedSum(v,c)
+            s=s+CountTokensOfTerm(text[c],token)
+        denominator = findSmoothedSum(v,c,t)
         for token in v:
             condProb[c+'.'+token] = (t[c+'.'+token]+1)/denominator
     return v, prior, condProb
 
+'''
+CountTokensOfTerm function counts the frequency of occurence of a given token in a given text
+'''
 def CountTokensOfTerm(text, token):
+    text_words= text.split(" ")
+    count=0
+    for w in text_words:
+        if w==token:
+            count=count+1
+    return count   
     
-    
-def findSmoothedSum(v,c):
-        
+'''
+findSmoothedSum finds the laplace smoothed sum used in denominator of condprob[t][c], given by ∑t′(Tct′+1)
+'''    
+def findSmoothedSum(v,c,t):
+    sum=0
+    for token in v:
+       sum = sum+ t[c+'.'+token]  +1 
+    return sum
+  
+'''
+this function does what it's name suggests for given document tuple D and class c
+'''      
 def ConcatenateTextOfAllDocsInClass(D,c):
     text=''
     if c=='ham':
@@ -68,7 +88,10 @@ def ConcatenateTextOfAllDocsInClass(D,c):
         for docs in D[1]:
             text = text + docs + " "
         return text
-        
+
+'''
+extractVocabulary function extracts the unique tokens given in D
+'''       
 def extractVocabulary(D):
     vocab=set()
     for entry in D[0]:
@@ -79,9 +102,15 @@ def extractVocabulary(D):
         vocab.update(set(word_list2))        
     return vocab
     
+'''
+countDocs counts number of documents in D
+'''
 def countDocs(D):
     return len(D[0])+len(D[1])
     
+'''
+countDocs counts number of documents in each class
+'''
 def countDocsInClass(D,c):
     if c=='ham':
         return len(D[0])
@@ -93,5 +122,10 @@ if __name__== '__main__':
     path_train_spam= '../dataSet1/train/spam'
     D= getDocs(path_train_ham, path_train_spam)
     C=['ham','spam']
-    TrainMultinomailNB(C,D)
+    res=TrainMultinomialNB(C,D)
+#    print(res[0])
+    print('=======================prior=========================')
+    print(res[1])
+    print('=======================conditional_probability=========================')
+    print(res[2])
    
